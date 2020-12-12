7b4cfe2388324e09569c92215a00aefc2ca836a6
Muhammad Al Faisal <fais.season2@gmail.com>
2020-12-12 11:25:43 +0700

    Make base application

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    println("Aplikasi Kalkulator")
+    var a:Int ?= 0
+    var b:Int ?= 0
+
+    println("=====================")
+    println("+Aplikasi Kalkulator+")
+    println("=====================")
+
 }
